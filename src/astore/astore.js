3562a15e6349179a019fc8ed8535892b472aa472
huangxueliang <huangxueliang@lbdfun.com>
2021-02-09 21:37:22 +0800

    feat: commit dispatch

--- src/astore/astore.js
+++ src/astore/astore.js
@@ -2,10 +2,33 @@ let Vue
 
 class Store {
   constructor(options) {
-    this.$options = options
+    // this.$options = options
+    this._mutations = options.mutations
+    this._actions = options.actions
     this.state = new Vue({
       data: options.state
     })
+
+    // 绑定commit、dispatch的上下文store实例
+    this.commit = this.commit.bind(this)
+    this.dispatch = this.dispatch.bind(this)
+  }
+
+  // store.commit('add', 1)
+  // type: mutation的类型
+  // payload: 载荷，是参数
+  commit(type, payload) {
+    const entry = this._mutations[type]
+    if (entry) {
+      entry(this.state, payload)
+    }
+  }
+
+  dispatch(type, payload) {
+    const entry = this._actions[type]
+    if (entry) {
+      entry(this, payload)
+    }
   }
 }
 
