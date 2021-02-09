a05361d063c9aeeed726b2ba2e0299ae1d573550
huangxueliang <huangxueliang@lbdfun.com>
2021-02-09 20:04:00 +0800

    feat: init

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <h1>home</h1>
+
+    <p @click="$store.commit('add')">count: {{ $store.state.count }}</p>
+    <p @click="$store.dispatch('add')">async count: {{ $store.state.count }}</p>
+    <p>double count: {{ $store.getters.doubleCount }}</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  methods: {
+    name() {
+      this.$store.dispatch('add')
+    }
+  }
+}
+</script>
